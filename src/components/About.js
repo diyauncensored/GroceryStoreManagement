710e57e3251be2e0ff7f7416d311b212fed6be38
diyauncensored <diyanandana@gmail.com>
2025-02-06 00:08:15 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
     return (
         <div className="about-container">
             <h2>About Us</h2>
-            <p>Done by Diya Nandana and Sankeerth P. Students of W1 section. </p>
+            <p>Done by Diya Nandana and Sankeerth P, Students of W1 section. </p>
             <p>Under the Guidance of Ms. Susheela</p>
         </div>
     );
